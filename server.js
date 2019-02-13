256be94d59d8e1ec0869e0e25277604c204df29c
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-02-13 18:34:14 -0500

    Finished MVP

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-
+const morgan = require("morgan");
 const usersRouter = require("./users/router");
 const postsRouter = require("./posts/router");
 
@@ -9,6 +9,7 @@ const server = express();
 server.use(express.json());
 server.use(cors());
 server.use(helmet());
+server.use(morgan("dev"));
 
 server.use("/api/users", usersRouter);
 server.use("/api/posts", postsRouter);
